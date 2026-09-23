5924d7db4763faa1203c93e987186b0cd2e9b450
wwcoderww <wwcoderww@gmail.com>
2026-09-23 12:11:18 -0500

    style(landingPage): add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import bgImage from './assets/mainBg.jpg';
+
 export default function App() {
   return (
-    <div>
+    <div
+      style={{ backgroundImage: `url(${bgImage})` }}
+      className="min-h-screen bg-cover bg-center"
+    >
       <div className="text-9xl text-amber-300">App</div>
     </div>
   );
