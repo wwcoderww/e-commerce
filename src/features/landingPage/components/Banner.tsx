6077d50683bc14145c699fa8503c3f9aff1a3483
wwcoderww <wwcoderww@gmail.com>
2026-09-23 13:28:47 -0500

    style(landingPage): minor banner styling

--- src/features/landingPage/components/Banner.tsx
+++ src/features/landingPage/components/Banner.tsx
@@ -1,16 +1,17 @@
-// import userImage from '../assets/user.svg';
+import userImage from '../assets/user.svg';
 export default function banner() {
   return (
-    <div className="flex justify-evenly text-2xl text-white">
+    <div className="flex justify-between px-2 py-1 text-2xl">
       <div>Company Logo</div>
-      <div>Home</div>
-      <div>Products</div>
-      <div>About</div>
-      <div>Contat Us</div>
-      <div>Search</div>
-      <div>
-        {/* <img src={userImage} className="w-10" alt="user" /> */}
-        Login
+      <div className="flex gap-10">
+        <div>Home</div>
+        <div>Products</div>
+        <div>About</div>
+        <div>Contat Us</div>
+        <div>Search</div>
+        <div>
+          <img src={userImage} className="w-10" alt="user" />
+        </div>
       </div>
     </div>
   );
