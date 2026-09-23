4afe491b716bfc6101535a55d148f16dd4969109
wwcoderww <wwcoderww@gmail.com>
2026-09-23 17:12:04 -0500

    style(landingPage): Text size

--- src/features/landingPage/LandingPage.tsx
+++ src/features/landingPage/LandingPage.tsx
@@ -3,17 +3,16 @@ import bgImage from './assets/mainBg.jpg';
 import mainPic from './assets/mainPic.jpg';
 
 export default function LandingPage() {
-  // Commit redo
   return (
     <div
       style={{ backgroundImage: `url(${bgImage})` }}
       className="flex min-h-screen max-w-screen flex-col bg-cover bg-center bg-no-repeat"
     >
       <Banner />
-      <div className="flex w-screen flex-1 gap-8 pt-10">
-        <div className="flex w-1/2 flex-col gap-8 px-10">
-          <div className="mt-[25vh] text-7xl">E-Commerce</div>
-          <div className="max-w-200 text-2xl">
+      <div className="flex w-screen flex-1 pt-10">
+        <div className="flex w-1/2 flex-col gap-12 px-10">
+          <div className="mt-[25vh] text-8xl">E-Commerce</div>
+          <div className="text-3xl">
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "The
             quick brown fox jumps over the lazy dog.", "React makes it painless
             to create interactive UIs.", "State updates tell React that a
