9e0f073e9b6b810768fe3779277f9b92651c2da4
wwcoderww <wwcoderww@gmail.com>
2026-09-23 13:43:53 -0500

    style(landingPage): minor

--- src/features/landingPage/components/Banner.tsx
+++ src/features/landingPage/components/Banner.tsx
@@ -2,12 +2,12 @@ import userImage from '../assets/user.svg';
 export default function banner() {
   return (
     <div className="flex justify-between px-2 py-1 text-2xl">
-      <div>Company Logo</div>
-      <div className="flex gap-10">
+      <div className="text-4xl">Company Logo</div>
+      <div className="flex items-center gap-10 font-medium">
         <div>Home</div>
         <div>Products</div>
         <div>About</div>
-        <div>Contat Us</div>
+        <div>Contact Us</div>
         <div>Search</div>
         <div>
           <img src={userImage} className="w-10" alt="user" />
