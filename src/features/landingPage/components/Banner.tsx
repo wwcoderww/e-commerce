7cdc5d5a7c3840f0f52f981595488cc3c31a04bf
wwcoderww <wwcoderww@gmail.com>
2026-09-23 14:30:49 -0500

    refactor(landingPage): export links to own file

--- src/features/landingPage/components/Banner.tsx
+++ src/features/landingPage/components/Banner.tsx
@@ -1,13 +1,11 @@
 import userImage from '../assets/user.svg';
+import Links from './Links';
 export default function banner() {
   return (
     <div className="flex justify-between px-2 py-1 text-2xl">
       <div className="text-4xl">Company Logo</div>
       <div className="flex items-center gap-10 font-medium">
-        <div>Home</div>
-        <div>Products</div>
-        <div>About</div>
-        <div>Contact Us</div>
+        <Links />
         <div>Search</div>
         <div>
           <img src={userImage} className="w-10" alt="user" />
---
+++ src/features/landingPage/components/Links.tsx
@@ -0,0 +1,10 @@
+export default function Links() {
+  return (
+    <div className="flex cursor-pointer gap-10">
+      <div>Home</div>
+      <div>Products</div>
+      <div>About</div>
+      <div>Contact Us</div>
+    </div>
+  );
+}
