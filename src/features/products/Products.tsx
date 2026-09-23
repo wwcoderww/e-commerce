ee868bbcb6500434f95817993dbe98a87dc9193c
wwcoderww <wwcoderww@gmail.com>
2026-09-23 17:47:06 -0500

    feat(Products): add product and product card frame

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,3 +1,23 @@
+import ProductCard from './components/ProductCard';
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <div className="flex flex-wrap justify-center gap-8">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  );
 }
---
+++ src/features/products/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+export default function ProductCard() {
+  return (
+    <div className="rounded-lg border-2 border-primary">
+      <div>Image</div>
+      <div>Title</div>
+      <div>Price / Rating</div>
+      <div>Brief Description</div>
+    </div>
+  );
+}
