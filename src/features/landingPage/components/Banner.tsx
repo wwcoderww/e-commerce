cb1d2b8f9e08942013e61813cb9ff699d6082059
wwcoderww <wwcoderww@gmail.com>
2026-09-23 16:45:07 -0500

    style(landingPage): Banner: Change size and name

--- src/features/landingPage/components/Banner.tsx
+++ src/features/landingPage/components/Banner.tsx
@@ -4,7 +4,7 @@ import Search from './Search';
 export default function banner() {
   return (
     <div className="flex items-center justify-between p-2 text-2xl">
-      <div className="text-4xl">Company Logo</div>
+      <div className="text-5xl">Company Name</div>
       <Links />
       <Search />
       <img src={userImage} className="w-12 cursor-pointer" alt="user" />
