f0ffeea155de3a49d21f0391ba2b95064adffa64
wwcoderww <wwcoderww@gmail.com>
2026-09-23 14:36:29 -0500

    feat(landingPage): banner: add <Link> to links

--- src/features/landingPage/components/Links.tsx
+++ src/features/landingPage/components/Links.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function Links() {
   return (
     <div className="flex cursor-pointer gap-10">
-      <div>Home</div>
-      <div>Products</div>
-      <div>About</div>
-      <div>Contact Us</div>
+      <Link to={'/'}>Home</Link>
+      <Link to={'/'}>Products</Link>
+      <Link to={'/'}>About</Link>
+      <Link to={'/'}>Contact Us</Link>
     </div>
   );
 }
