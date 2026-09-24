aa3806ebf04e2b14aa1fbc5d6e47e868eba57d38
wwcoderww <wwcoderww@gmail.com>
2026-09-24 10:28:08 -0500

    feat(Products): Retrive temporary products and render as ProductCard

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,23 +1,26 @@
+'use client';
+import { Suspense, useEffect, useState } from 'react';
 import ProductCard from './components/ProductCard';
+import type { Product } from '../../types/Product';
 
 export default function Products() {
+  const [data, setData] = useState<Product[] | null>(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="flex flex-wrap justify-center gap-8">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      <Suspense fallback={<div>Loading...</div>}>
+        {data.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Suspense>
     </div>
   );
 }
