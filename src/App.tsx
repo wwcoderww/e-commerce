22f33f6f21fc26433de6ae11d0619c23a255c395
wwcoderww <wwcoderww@gmail.com>
2026-09-25 09:17:50 -0500

    refactor(Main/App): Change querryclient and redux provider to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import maingBg from './assets/mainBg.jpg';
 import Banner from './components/Banner';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60,
-      },
-    },
-  });
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <div
-        style={{ backgroundImage: `url(${maingBg})` }}
-        className="flex min-h-screen flex-col bg-cover bg-center bg-no-repeat font-sans text-primary subpixel-antialiased"
-      >
-        <Banner />
-        <Outlet />
-      </div>
-    </QueryClientProvider>
+    <div
+      style={{ backgroundImage: `url(${maingBg})` }}
+      className="flex min-h-screen flex-col bg-cover bg-center bg-no-repeat font-sans text-primary subpixel-antialiased"
+    >
+      <Banner />
+      <Outlet />
+    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import './index.css';
 import LandingPage from './features/landingPage/LandingPage.tsx';
 import App from './App.tsx';
 import Products from './features/products/Products.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
+import { store } from './hooks/store.ts';
 
 const router = createBrowserRouter([
   {
@@ -20,8 +24,21 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 );
