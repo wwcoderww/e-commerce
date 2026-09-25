d9a971b1c2e076602b5ebb305d2f363246b7a409
wwcoderww <wwcoderww@gmail.com>
2026-09-25 14:46:21 -0500

    style(banner): add hover/opacity effect to links

--- src/features/banner/components/Links.tsx
+++ src/features/banner/components/Links.tsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
 
+const linkClasses =
+  'transition duration-300 ease-in-out hover:-translate-y-0.5 hover:!opacity-100 group-hover:opacity-70';
+
 export default function Links() {
   return (
-    <div className="flex flex-1 justify-end gap-10 px-6">
-      <Link to={'/'}>Home</Link>
-      <Link to={'/products'}>Products</Link>
-      <Link to={'/about'}>About</Link>
-      <Link to={'/'}>Contact Us</Link>
+    <div className="group flex flex-1 justify-end gap-10 px-6">
+      <Link className={linkClasses} to={'/'}>
+        Home
+      </Link>
+      <Link className={linkClasses} to={'/products'}>
+        Products
+      </Link>
+      <Link className={linkClasses} to={'/about'}>
+        About
+      </Link>
+      <Link className={linkClasses} to={'/'}>
+        Contact Us
+      </Link>
     </div>
   );
 }
